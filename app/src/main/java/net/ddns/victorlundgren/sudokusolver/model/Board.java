7424c4b276c944f090cae08f366489e2278e7d09
Victor Lundgren <victor.lundgren@outlook.com>
2021-02-19 11:54:13 +0100

    Added Board that holds the sudoku board

--- app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Board.java
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Board.java
@@ -1,4 +1,25 @@
 package net.ddns.victorlundgren.sudokusolver.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Board {
+    private static final int ROWS = 9;
+    private int[] cells;
+
+    public Board(int[] cells) {
+        this.cells = cells;
+    }
+
+    public int[] getCells() {
+        return cells;
+    }
+
+    public int getCell(Position p) {
+        return cells[p.getX()*ROWS + p.getY()];
+    }
+
+    public void setCells(int cell, Position p) {
+        cells[p.getX()*ROWS + p.getY()] = cell;
+    }
 }
