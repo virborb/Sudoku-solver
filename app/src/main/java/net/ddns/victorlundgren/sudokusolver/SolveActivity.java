9e61e4baff21dad90b5aa66875585d5500bb863f
Victor Lundgren <victor.lundgren@outlook.com>
2021-03-02 11:56:34 +0100

    Solver can now check if a cell block is correct

--- app/src/main/java/net/ddns/victorlundgren/sudokusolver/SolveActivity.java
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/SolveActivity.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 
+import net.ddns.victorlundgren.sudokusolver.model.Board;
+import net.ddns.victorlundgren.sudokusolver.model.Solver;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SolveActivity extends AppCompatActivity {
@@ -20,12 +24,16 @@ public class SolveActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        List<Integer> cells = getIntent().getIntegerArrayListExtra(InsertActivity.CELL_ARRAY);
+        ArrayList<Integer> cells = getIntent().getIntegerArrayListExtra(InsertActivity.CELL_ARRAY);
         List<Fragment> list = getSupportFragmentManager().getFragments();
+
+        Board board = new Board(cells);
+        Solver solver = new Solver(board);
+        solver.solveSudoku();
         for (Fragment f: list) {
             CellGroupFragment cellGroup = (CellGroupFragment) f;
             cellGroup.setCells(cells);
-            cells = cells.subList(9, cells.size());
+            cells = new ArrayList<Integer>(cells.subList(9, cells.size()));
         }
     }
 }
--- app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
@@ -1,5 +1,10 @@
 package net.ddns.victorlundgren.sudokusolver.model;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solver {
     private Board board;
 
@@ -11,13 +16,47 @@ public class Solver {
         return false;
     }
 
-    public Board solvSudoku() {
-        if(isLegalBoard()) {
-            int[] cells = board.getCells();
-            for (int cell : cells) {
-
-            }
+    public Board solveSudoku() {
+        ArrayList<Integer> cells = board.getCells();
+        for (int i = 0; i < 9; i++) {
+            Log.d("MyTag", "solveSudoku: " + cells.subList(i * Board.ROWS, i * Board.ROWS + 9).toString());
+            checkCellBlock(new ArrayList<Integer>(cells.subList(i * Board.ROWS, i * Board.ROWS + 9)));
         }
         return board;
+    }
+
+    private boolean checkSuduku(ArrayList<Integer> cells) {
+        if(!checkCellBlock(cells)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkCellBlock(ArrayList<Integer> cells) {
+        ArrayList<Integer> check =
+                new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        for (int i = 0; i < 9; i++) {
+            Integer temp = cells.get(i);
+            if(check.contains(temp)) {
+                check.remove(temp);
+            } else if (temp != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkRow(ArrayList<Integer> cells) {
+        ArrayList<Integer> check =
+                new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        for (int i = 0; i < 9; i++) {
+            Integer temp = cells.get(i);
+            if(check.contains(temp)) {
+                check.remove(temp);
+            } else if (temp != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
