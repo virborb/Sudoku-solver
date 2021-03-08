7ca8509c86df6542a37b8f1d54cd4dec701dc852
Victor Lundgren <victor.lundgren@outlook.com>
2021-03-08 11:00:33 +0100

    Changed so metod checks all rows instead

--- app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
@@ -18,31 +18,31 @@ public class Solver {
 
     public Board solveSudoku() {
         ArrayList<Integer> cells = board.getCells();
-        for (int i = 0; i < 9; i++) {
-            Log.d("MyTag", "solveSudoku: " + cells.subList(i * Board.ROWS, i * Board.ROWS + 9).toString());
-            Log.d("MyTag", "solveSudoku: " + checkRow(new ArrayList<Integer>(cells.subList(i * Board.ROWS, i * Board.ROWS + 9))));
-        }
+        Log.d("MyTag", "checkRows return: " + checkRows(cells));
         Log.d("MyTag", "checkColumns return: " + checkColumns(cells));
         return board;
     }
 
     private boolean checkSuduku(ArrayList<Integer> cells) {
-        if(!checkRow(cells)) {
+        if(!checkRows(cells)) {
             return false;
         }
         return true;
     }
 
-    private boolean checkRow(ArrayList<Integer> cells) {
+    private boolean checkRows(ArrayList<Integer> cells) {
         ArrayList<Integer> check =
                 new ArrayList<>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int i = 0; i < 9; i++) {
-            Integer temp = cells.get(i);
-            if(check.contains(temp)) {
-                check.remove(temp);
-            } else if (temp != 0) {
-                return false;
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLUMNS; j++) {
+                Integer temp = cells.get(i * Board.ROWS + j);
+                if (check.contains(temp)) {
+                    check.remove(temp);
+                } else if (temp != 0) {
+                    return false;
+                }
             }
+            check = new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
         }
         return true;
     }
@@ -64,25 +64,25 @@ public class Solver {
         return true;
     }
 
-    private boolean checkRows(ArrayList<Integer> cells) {
-
-        ArrayList<Integer> check =
-                new ArrayList<>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int i = 0; i < cells.size()-1; i++) {
-            if(i != 0 && i%22 == 0) {
-                i = i - 19;
-                check = new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
-            } else {
-                i = (i != 0 && i % 3 == 0) ? i + 6 : i;
-            }
-            Integer temp = cells.get(i);
-            Log.d("MyTag", "checkRow: " + temp + " i: " + i);
-            if(check.contains(temp)) {
-                check.remove(temp);
-            } else if (temp != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+//    private boolean checkRows(ArrayList<Integer> cells) {
+//
+//        ArrayList<Integer> check =
+//                new ArrayList<>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+//        for (int i = 0; i < cells.size()-1; i++) {
+//            if(i != 0 && i%22 == 0) {
+//                i = i - 19;
+//                check = new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+//            } else {
+//                i = (i != 0 && i % 3 == 0) ? i + 6 : i;
+//            }
+//            Integer temp = cells.get(i);
+//            Log.d("MyTag", "checkRow: " + temp + " i: " + i);
+//            if(check.contains(temp)) {
+//                check.remove(temp);
+//            } else if (temp != 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
