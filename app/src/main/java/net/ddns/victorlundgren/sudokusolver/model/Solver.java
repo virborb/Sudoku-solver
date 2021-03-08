7de36528430c62a3b901d4dcb56a62282897251d
Victor Lundgren <victor.lundgren@outlook.com>
2021-03-08 10:29:01 +0100

    Now checks if a row is correct

--- app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/model/Solver.java
@@ -20,21 +20,22 @@ public class Solver {
         ArrayList<Integer> cells = board.getCells();
         for (int i = 0; i < 9; i++) {
             Log.d("MyTag", "solveSudoku: " + cells.subList(i * Board.ROWS, i * Board.ROWS + 9).toString());
-            checkCellBlock(new ArrayList<Integer>(cells.subList(i * Board.ROWS, i * Board.ROWS + 9)));
+            Log.d("MyTag", "solveSudoku: " + checkRow(new ArrayList<Integer>(cells.subList(i * Board.ROWS, i * Board.ROWS + 9))));
         }
+        //Log.d("MyTag", "checkRows return: " + checkRows(cells));
         return board;
     }
 
     private boolean checkSuduku(ArrayList<Integer> cells) {
-        if(!checkCellBlock(cells)) {
+        if(!checkRow(cells)) {
             return false;
         }
         return true;
     }
 
-    private boolean checkCellBlock(ArrayList<Integer> cells) {
+    private boolean checkRow(ArrayList<Integer> cells) {
         ArrayList<Integer> check =
-                new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                new ArrayList<>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
         for (int i = 0; i < 9; i++) {
             Integer temp = cells.get(i);
             if(check.contains(temp)) {
@@ -46,11 +47,19 @@ public class Solver {
         return true;
     }
 
-    private boolean checkRow(ArrayList<Integer> cells) {
+    private boolean checkRows(ArrayList<Integer> cells) {
+
         ArrayList<Integer> check =
-                new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int i = 0; i < 9; i++) {
+                new ArrayList<>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        for (int i = 0; i < cells.size()-1; i++) {
+            if(i != 0 && i%22 == 0) {
+                i = i - 19;
+                check = new ArrayList<Integer>(Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9));
+            } else {
+                i = (i != 0 && i % 3 == 0) ? i + 6 : i;
+            }
             Integer temp = cells.get(i);
+            Log.d("MyTag", "checkRow: " + temp + " i: " + i);
             if(check.contains(temp)) {
                 check.remove(temp);
             } else if (temp != 0) {
