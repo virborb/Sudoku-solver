66bd2a9d041e4fc726fba57973f74af83a445c95
Victor Lundgren <victor.lundgren@outlook.com>
2021-03-19 10:15:27 +0100

    Added a Scan Activity

---
+++ app/src/main/java/net/ddns/victorlundgren/sudokusolver/ScanActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.victorlundgren.sudokusolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
